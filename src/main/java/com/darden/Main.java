4c090c2b9c238e4acbe471e2c2d176d35d3c98f0
Shankarjj <104765154+Shankarjj@users.noreply.github.com>
2023-02-02 15:38:56 +0530

    Initial commit

---
+++ src/main/java/com/darden/Main.java
@@ -0,0 +1,7 @@
+package com.darden;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/TestLogic.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+//import org.junit.Test;
+public class TestLogic {
+    @Test
+    public void testFindMax(){
+        assertEquals(4,4);
+        assertEquals(-1,-1);
+    }
+}
